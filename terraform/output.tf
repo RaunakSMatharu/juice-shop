3ccc9dad1a4225f91640621cb89a6b130b881f71
RaunakSMatharu <matharu.r@northeastern.edu>
2025-01-07 11:01:28 -0500

    disable docker buld

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,3 @@
-output "rg_name" {
-  description = "Resource Group Name."
-  value       = azurerm_resource_group.rg.name
-}
-
 output "app_fqdn" {
   description = "Fully Qualified Domain Name of the Application."
   value       = module.counting.fqdn_url
